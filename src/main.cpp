3c9b88027b6dd89671a1d830ede6cec959051fab
K4mp47 <campagnoloalberto5@gmail.com>
2023-05-31 22:22:27 +0200

    add get and is method, started in stream

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include "../include/json.hpp" 
+#include <istream>
+#include <sstream>
+
+enum Type { Boolean, Number, Null, Dict, List, String };
 
 struct json::impl {
   bool b_value;
+  
   double d_value;
+  
   std::string s_value;
-  // tipo lista
-  // tipo dizionario
-  // tipo null 
+  
+  struct list {
+    json info;
+    list* next;
+  };
+  
+  list* list_head;
+  list* list_tail;
+
+  struct dict{
+    std::pair<std::string, json> info;
+    dict* next;
+  };
+  
+  dict* dict_head;
+  dict* dict_tail;
+  
+  Type type;
 };
 
 json::json() { // constructor
@@ -28,29 +49,108 @@ json::~json(){
 }
 
 bool json::is_list() const {
-  return true;
+  return pimpl->type == Type::List;
 }
 
 bool json::is_dictionary() const {
-  return true;
+  return pimpl->type == Type::Dict;
+}
+
+// string control
+
+std::string& json::get_string() {
+  if(!is_string()){
+    throw json_exception{ .msg = "json is not a string" };
+  }
+
+  return pimpl->s_value;
+}
+
+std::string const& json::get_string() const {
+ if(!is_string()){
+    throw json_exception{ .msg = "json is not a string" };
+  }
+
+  return pimpl->s_value;
 }
 
 bool json::is_string() const {
-  return true;
+  return pimpl->type == Type::String;
 }
+
+// number control
+
+double& json::get_number() {
+  if(!is_number()){
+    throw json_exception{ .msg = "json is not a double"};
+  }
+
+  return pimpl->d_value;
+}
+
+double const& json::get_number() const {
+ if(!is_number()){
+    throw json_exception{ .msg = "json is not a double"};
+  }
+
+  return pimpl->d_value;
+}
+
+
 bool json::is_number() const {
-  return true;
+  return pimpl->type == Type::Number;
+}
+
+// bool control
+
+bool& json::get_bool(){
+  if(!is_bool()) {
+    throw json_exception{ .msg = "json is not a boolean" };
+  } 
+
+  return pimpl->b_value;
+}
+
+bool const& json::get_bool() const {
+  if(!is_bool()) {
+    throw json_exception{ .msg = "json is not a boolean" };
+  }
+
+  return pimpl->b_value;
 }
 
 bool json::is_bool() const {
-  return true;
+  return pimpl->type == Type::Boolean;
 }
+
+// null control
 
 bool json::is_null() const{
-  return true;
+  return pimpl->type == Type::Null;
 }
 
 
+// void parse_string()
+//  std::string str;
+// 
+// while(lhs.peek() != "" || lhs.peek() != EOF)
+//  str += lhs.get();
+//
+//  rhs.set_string(str);
+
+
+//put_back(cosa voglio in char) ributta nella stream quello che voglio
+
+std::istream& operator>>(std::istream& hs, json& rhs) {
+  if(hs.get() == '"'){
+    parse_string(hs, rhs);
+  } else if(hs.get() >= 0 && hs.get() <= 9) {
+
+  }
+
+}
+
 int main(){
+
   return 0;
 }
